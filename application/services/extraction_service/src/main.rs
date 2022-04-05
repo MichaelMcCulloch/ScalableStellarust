4add6633e206340c574a31ec0ee3575544b3f8cc
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-04-05 16:18:01 -0600

    init a bunch of sub projects

---
+++ application/services/extraction_service/src/main.rs
@@ -0,0 +1,23 @@
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Rust service prototype")
+}
+
+#[get("/healthcheck")]
+async fn healthcheck() -> impl Responder {
+    HttpResponse::Ok().body("I'm alive!")
+}
+
+pub fn init(config: &mut web::ServiceConfig) {
+    config.service(web::scope("").service(index).service(healthcheck));
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().configure(init))
+        .bind("0.0.0.0:8000")?
+        .run()
+        .await
+}
