967504faa5b388b54d843e9573eeee994a0ce2e1
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-04-16 13:36:27 -0600

    make unzipping service depend on a config file

--- application/services/unzipping_service/src/main.rs
+++ application/services/unzipping_service/src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, middleware, web, App, HttpResponse, HttpServer, Responder};
+use unzipping_service::config;
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -16,8 +17,24 @@ pub fn init(config: &mut web::ServiceConfig) {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().configure(init))
-        .bind("0.0.0.0:8002")?
-        .run()
-        .await
+    let config_toml = std::fs::read_to_string("Config.toml")?;
+    let config: config::Config = toml::from_str(&config_toml)?;
+    std::env::set_var(
+        "RUST_LOG",
+        format!(
+            "actix_web={},webapp={}",
+            config.log.actix_web, config.log.service
+        ),
+    );
+    env_logger::init();
+
+    let bind_address = format!("{}:{}", config.service.server, config.service.port);
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .configure(init)
+    })
+    .bind(bind_address)?
+    .run()
+    .await
 }
