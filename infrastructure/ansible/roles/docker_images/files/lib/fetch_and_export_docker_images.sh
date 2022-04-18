f5fcc941cfeafd0fd4011c3f91a24c03958ced7d
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-04-17 18:02:36 -0600

    STAR-21 - populate with stellarust images

--- infrastructure/ansible/roles/docker_images/files/lib/fetch_and_export_docker_images.sh
+++ infrastructure/ansible/roles/docker_images/files/lib/fetch_and_export_docker_images.sh
@@ -5,11 +5,11 @@ REGISTRY=registry:2.8.1
 RUST_BUILDER=ekidd/rust-musl-builder:latest
 MONGO=mongo:5.0.7-focal
 
-for i in  "$NODE node" "$REGISTRY registry" "$RUST_BUILDER rust_builder" "$MONGO mongo" ; do
-    a=( $i );
-    docker pull "${a[0]}"
-    docker save ${a[0]} > ${a[1]}.tar
-    xz -T32 -9 ${a[1]}.tar
+for pairs in  "$NODE node" "$REGISTRY registry" "$RUST_BUILDER rust_builder" "$MONGO mongo" ; do
+    pair=( $pairs );
+    docker pull "${pair[0]}"
+    docker save ${pair[0]} > ${pair[1]}.tar
+    xz -T32 -9 ${pair[1]}.tar
 done
 
 
