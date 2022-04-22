568bd217db06700da1027b761b2b84261591de94
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-04-21 21:01:36 -0600

    STAR-35 - also generate the sums for hub images

--- infrastructure/ansible/roles/docker_images/files/lib/fetch_and_export_docker_images.sh
+++ infrastructure/ansible/roles/docker_images/files/lib/fetch_and_export_docker_images.sh
@@ -5,11 +5,12 @@ NODE=node:17.9.0-alpine3.15
 RUST_BUILDER=ekidd/rust-musl-builder:latest
 MONGO=mongo:5.0.7-focal
 
-for pairs in  "$NODE node" "$REGISTRY registry" "$RUST_BUILDER rust_builder" "$MONGO mongo" ; do
+for pairs in "$NODE node" "$REGISTRY registry" "$RUST_BUILDER rust_builder" "$MONGO mongo"; do
     pair=( $pairs );
     docker pull "${pair[0]}"
     docker save ${pair[0]} > ${pair[1]}.tar
     xz -T32 -9 ${pair[1]}.tar
+    sha256sum ${pair[1]}.tar.xz > ${pair[1]}.sha256
 done
 
 
